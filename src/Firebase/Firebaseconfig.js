19210c4c402654ef7baa85fd747450c5886629c1
Albert Casas <albertcasas3d@gmail.com>
2022-10-31 12:31:48 +0100

    variables

--- src/Firebase/Firebaseconfig.js
+++ src/Firebase/Firebaseconfig.js
@@ -3,12 +3,12 @@ import 'firebase/compat/firestore';
 
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBpFva1QAG9xudY89nAteiJ8EqiJbaauUs",
-    authDomain: "pantera-studio-84d0c.firebaseapp.com",
+    apiKey: import.meta.env.VITE_KEY,
+    authDomain: import.meta.env.AUTH_DOMAIN,
     projectId: "pantera-studio-84d0c",
-    storageBucket: "pantera-studio-84d0c.appspot.com",
-    messagingSenderId: "585438187657",
-    appId: "1:585438187657:web:33661c4f7d9b4269f5b658",
+    storageBucket: import.meta.env.STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.MESSAGIG_SENDER_ID,
+    appId: import.meta.env.APP_ID,
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
