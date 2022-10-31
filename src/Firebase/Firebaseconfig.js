52557c902e4ec4d7a40f467f5e8831b45ec6707a
Albert Casas <albertcasas3d@gmail.com>
2022-10-31 12:22:33 +0100

    carousel

--- src/Firebase/Firebaseconfig.js
+++ src/Firebase/Firebaseconfig.js
@@ -9,7 +9,6 @@ const firebaseConfig = {
     storageBucket: "pantera-studio-84d0c.appspot.com",
     messagingSenderId: "585438187657",
     appId: "1:585438187657:web:33661c4f7d9b4269f5b658",
-    measurementId: "G-H8Y8F2L354"
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
